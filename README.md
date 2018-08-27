4d86ea65b2db1da07c567934cac415c03e158db3
wupeixuan <wupx@missfresh.cn>
2018-08-27 17:13:21 +0800

    867. Transpose Matrix（转置矩阵）

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 1|[两数之和](http://www.cnblogs.com/wupeixuan/p/8825617.html)|简单
 53|[最大子序和](http://www.cnblogs.com/wupeixuan/p/8721518.html)|简单
 709|[转换成小写字母](https://www.cnblogs.com/wupeixuan/p/9541259.html)|简单
-709|[宝石与石头](https://www.cnblogs.com/wupeixuan/p/9541094.html)|简单
+771|[宝石与石头](https://www.cnblogs.com/wupeixuan/p/9541094.html)|简单
+867|[转置矩阵](https://www.cnblogs.com/wupeixuan/p/9543358.html)|简单
 
 
--- src/Array/Solution867.java
+++ src/Array/Solution867.java
@@ -1,4 +1,32 @@
 package Array;
 
+/**
+ * 867. Transpose Matrix（转置矩阵）
+ * 给定一个矩阵 A， 返回 A 的转置矩阵。
+ * 矩阵的转置是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+ */
 public class Solution867 {
+    public static void main(String[] args) {
+        Solution867 solution867 = new Solution867();
+        int[][] A = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] res = solution867.transpose(A);
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[i].length; j++) {
+                System.out.print(res[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] transpose(int[][] A) {
+        int col = A.length;
+        int row = A[0].length;
+        int[][] res = new int[row][col];
+        for(int i = 0;i < row;i++){
+            for(int j = 0;j < col;j++){
+                res[i][j] = A[j][i];
+            }
+        }
+        return res;
+    }
 }
